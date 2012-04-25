48c3f8f5cd17c52a5d511e0c48d97b2401005260
Patrick Gombert <patrickgombert@gmail.com>
2012-04-25 10:09:28 -0500

    Refactor AI Special Cases

--- src/ai/special_case.erl
+++ src/ai/special_case.erl
@@ -9,25 +9,25 @@ check(Board, AICharacter, OpponentCharacter) ->
   end.
 
 three_dimension_cases(Board, AICharacter, OpponentCharacter) ->
-  Fork = check_fork(Board, AICharacter, OpponentCharacter),
-  FirstMove = check_first_move(Board),
-  case Fork of
-    {true, _} -> Fork;
-    false ->
-        case FirstMove of
-          true -> {true, {1, 1}};
-          false -> false
-        end
+  {ForkCase, Fork} = check_fork(Board, AICharacter, OpponentCharacter),
+  {FirstMoveCase, FirstMove} = check_first_move(Board),
+  if
+    ForkCase -> {true, Fork};
+    FirstMoveCase -> {true, FirstMove};
+    true -> false
   end.
 
 check_fork(Board, AICharacter, OpponentCharacter) ->
   case board_matches_fork(Board, AICharacter, OpponentCharacter) of
     true -> {true, {1, 2}};
-    false -> false
+    false -> {false, no_match}
   end.
 
 check_first_move(Board) ->
-  Board == board:create(3).
+  case Board == board:create(3) of
+    true -> {true, {1, 1}};
+    false -> {false, no_match}
+  end.
 
 board_matches_fork(Board, AI, Opponent) ->
   Empty = game_record:untaken_space(),
